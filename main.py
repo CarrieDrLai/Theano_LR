5b5f5c4d50d7f74ebc23eb9554fabb3f751ddb89
CarrieDrLai <41772585+CarrieDrLai@users.noreply.github.com>
2020-08-09 20:40:42 +0800

    Add files via upload

--- main.py
+++ main.py
@@ -5,14 +5,15 @@ Created on Thu Jul 23 11:44:23 2020
 @author: CarrieLai
 """
 
-from get_dataset import GetHoopPos,GetDataset,create_annotation
+# from get_dataset import GetHoopPos,GetDataset,create_annotation
 from load_dataset import load_dataset
-from extract_feature import extract_feature
-from lr_msgd import MSGD_LogisticRegression
+# from extract_feature import extract_feature,extract_feature_using_package
+# from lr_msgd import MSGD_LogisticRegression
 from data_separate import SeperateData, get_feature_2frame, import_data_seperated, shuffle_data
+from lib_using import convert_format, draw_roc
 #from naive_classifier import Predict
 
-import numpy as np
+import os
 import matplotlib.pyplot as plt
 
 #############################   Read Congig File   #############################
@@ -34,7 +35,9 @@ for var in config:
     if var_name == 'training_set' or var_name == 'epoch' or var_name == 'learning_rate' or var_name == 'batch_size' or var_name == 'lamda' or var_name == 'improve_thresh':
         var_value = eval(var_value)    
     if var_name == 'frame' or var_name == 'line_search' or var_name == 'weight_decay' or var_name == 'early_stop':
-        var_value = eval(var_value)    
+        var_value = eval(var_value) 
+    if var_name == 'c' or var_name == 't' or var_name == 'g' or var_name == 'd':
+        var_value = eval(var_value) 
     var_list.append([var_name, var_value])
     print( " " + var_name + " = " + str(var_value))
     
@@ -102,7 +105,7 @@ elif task_type == 'annotation':
 
     create_annotation(dir_pos, dir_neg, path_annotation, save_label_npy, save_data_npy, crop_size, colume)
 
-###########################    Step 2 : Train Model   ##############################
+###########################    Step 2 : Extract Feature   ##############################
 
 elif task_type == 'feature':
 
@@ -110,7 +113,6 @@ elif task_type == 'feature':
     fn_annotation = var_list["fn_annotation"]
     path_annotation = ".\\sample\\" + fn_annotation
     fn_feature = var_list["fn_feature"]
-    dir_save_feature = var_list["dir_save_feature"]
     
     block_size = var_list["block_size"]
     block_stride = var_list["block_stride"]
@@ -121,67 +123,152 @@ elif task_type == 'feature':
 
     Dataset = load_dataset(path_annotation,path_data,crop_size)
     data, label = Dataset.load_data() 
-    #X_train,X_test, y_train, y_test = train_test_split(data,label,test_size=0.3, random_state=0)
 
     ###############  (b)Extract Feature  
 
-    hog = extract_feature(dir_save_feature, fn_feature, data, block_size, block_stride, cell_size, bin_num)
-    feature = hog.HoG_output_vector()
+#    hog = extract_feature(dir_save_feature, fn_feature, data, block_size, block_stride, cell_size, bin_num)
+#    feature = hog.HoG_output_vector()
     
-    ################## 2020-08-05 ########################
+    feature = extract_feature_using_package(data,dir_save_feature,fn_feature)
 
-    fn_dataset = ["X_train", "X_test", "y_train", "y_test"]
-    fn_dataset_2frame = ["X_train_2frame", "X_test_2frame", "y_train_2frame", "y_test_2frame"]
+
+elif task_type == 'seperate':
     
+    dir_save_feature = var_list["dir_save_feature"]
+    
+    fn_dataset = ["X_train", "X_test", "y_train", "y_test"]
     SeperateData(dir_save_feature, fn_dataset,flag = 1)
+
+    fn_dataset_2frame = ["X_train_2frame", "X_test_2frame", "y_train_2frame", "y_test_2frame"]
     get_feature_2frame(dir_save_feature)
     SeperateData(dir_save_feature,fn_dataset_2frame,flag = 2)
     
-    ################## 2020-08-05 ########################
+    fn_dataset_cross = ["X_train_part", "X_test_part", "y_train_part", "y_test_part"]
+    SeperateData(dir_save_feature,fn_dataset_cross,flag = 3)
+    
 
 elif task_type == 'classification':
     
     dir_save_feature = var_list["dir_save_feature"]
 
     ##############  (c)Fit Model & Predict  
-    
-    training_set = var_list["training_set"]
-    n_epochs = var_list["epoch"]
-    lr = var_list["learning_rate"]
-    batch_size = var_list["batch_size"]
-    lamda = var_list["lamda"]
-    improve_thresh = var_list["improve_thresh"]
 
-    frame = var_list["frame"]
-    line_search = var_list["line_search"]
-    weight_decay = var_list["weight_decay"]
-    early_stop = var_list["early_stop"]    
-    
-    ################# 2020-08-04 ########################
-    
-    fn_dataset = ["X_train", "X_test", "y_train", "y_test"]
     fn_dataset_2frame = ["X_train_2frame", "X_test_2frame", "y_train_2frame", "y_test_2frame"]
-
+    
 #    X_train, X_test, y_train, y_test = import_data_seperated(dir_save_feature,fn_dataset)
     X_train, X_test, y_train, y_test = import_data_seperated(dir_save_feature,fn_dataset_2frame)
     # after normalization
     X_train, y_train = shuffle_data(X_train, y_train)
     X_test, y_test = shuffle_data(X_test, y_test)
     
-    ################## 2020-08-04 ########################
+    
+####################################
+####### Logistic Regression
+####################################
 
-#    from sklearn.linear_model import LogisticRegression
-#    model = LogisticRegression(max_iter=1000)
-#    model.fit(X_train, y_train)
+#    training_set = var_list["training_set"]
+#    n_epochs = var_list["epoch"]
+#    lr = var_list["learning_rate"]
+#    batch_size = var_list["batch_size"]
+#    lamda = var_list["lamda"]
+#    improve_thresh = var_list["improve_thresh"]
+#
+#    frame = var_list["frame"]
+#    line_search = var_list["line_search"]
+#    weight_decay = var_list["weight_decay"]
+#    early_stop = var_list["early_stop"]    
+#
 #    set_num = [1000, 2000, 4000, 32000, 48000, 60000]
 #    for i in range(6):
 #        training_set = set_num[i]
 #    idx = np.random.randint(0,np.shape(X_train)[0],training_set)
 #    X_train_part = X_train[idx]
 #    y_train_part = y_train[idx]
-    
-#    model = MSGD_LogisticRegression(X_train_part, y_train_part, X_test, y_test, lr, batch_size, n_epochs, lamda, improve_thresh, early_stop,weight_decay, n_class=2)
+##    
+#    model = MSGD_LogisticRegression(X_train, y_train, X_test, y_test, lr, batch_size, n_epochs, lamda, improve_thresh, early_stop,weight_decay, n_class=2)
+##    model.load_model()
 #    model.msgd_optimization()
+#    model.predict()
 #    model.draw_ROC()
 #    model.save_model()
+    
+####################################
+####### libsvm
+####################################
+
+    from libsvm import svmutil
+    from svmutil import *
+    import gc
+    c = var_list["c"]  
+    t = var_list["t"]   # 0: linear; 1 : polynomial; default : 2, rbf kernel
+    #g = var_list["g"]   #gamma
+    g = [0.001,0.01,0.1]
+    #d = var_list["d"]   #degree
+    print(" >>>> Convert y format !!!!")
+    y_train = convert_format(y_train)
+    y_test = convert_format(y_test)
+    
+    for i in range(len(g)):
+        
+        #parameter = "-s 0" + " -t " + str(t) + " -c " + str(c)
+        parameter = "-s 0" + " -t " + str(t) + " -c " + str(c) + " -g " + str(g[i])
+        #parameter = "-s 0" + " -t " + str(t) + " -c " + str(c) + " -d " + str(d) + " -g " + str(g)
+        
+        if os.path.exists(".\\roc\\libsvm " + parameter + " Fea_2.npy"):
+            print(" >>>> File is already exist.")
+            #continue
+        else:
+            print(" >>>> Get parameter !!!!")
+            param = svm_parameter(parameter)
+            prob = svm_problem(y_train, X_train)
+            
+            print(" >>>> Start Training !!!!")
+            model = svm_train(prob, param)
+            svm_save_model('model/' + parameter + '.model', model)
+            print(" >>>> Training Done !!!!")
+        #    del y_train, x_train, prob
+        #    gc.collect()
+        
+            print(" >>>> Start Testing !!!!")
+            p_label, p_acc, p_val = svm_predict(y_test, X_test, model)
+            # print(p_acc)
+            print(parameter + ' acc = %f' % p_acc[0])
+            draw_roc(y_test, p_val, parameter, flag = 1)
+
+
+
+####################################
+####### liblinear
+####################################
+
+#    import liblinear
+#    import liblinearutil as util
+#    import liblinear_commonutil
+#    
+#    y_train = convert_format(y_train)
+#    y_test = convert_format(y_test)
+#    #parameter = '-c 0.5 -v 5'
+#    c = [0.5, 1]
+#    for i in range(len(c)):
+#        parameter = '-c ' + str(c[i])
+#        if os.path.exists(".\\roc\\liblinear " + parameter + " Fea_2.npy"):
+#            print(" >>>> File is already exist.")
+#        else:
+#            print(" >>>> Get parameter !!!!")
+#            
+#            para = util.parameter(parameter)
+#            prob = util.problem(y_train,X_train)
+#        
+#            print(" >>>> Start Training !!!!")
+#            model = util.train(prob, para)
+#            print(" >>>> Training Done !!!!")
+#            # m = train(y, x, '-c 5')
+#            # m = train(prob, '-w1 5 -c 5')
+#            # m = train(prob, param)
+#            # CV_ACC = train(y, x, '-v 3')
+#        
+#            p_label, p_acc, p_val = util.predict(y_test, X_test, model)
+#            print(parameter + ' acc = %f' % p_acc[0])
+#            draw_roc(y_test, p_val, parameter, flag = 2)
+
 
